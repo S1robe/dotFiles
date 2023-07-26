24416ef86612ea23ed3b4bde8dbb6079c515b342
Strobe <GarrettPrentice@tuta.io>
2023-07-26 15:11:09 -0700

    added bookmark binary

--- dropscript.sh
+++ dropscript.sh
@@ -13,3 +13,14 @@ for i in ./.*
 do
 	ln -si "$PWD/${i##*/}" "$HOME/${i##*/}"
 done
+
+cd ../binaries
+
+if ! [ -f "$HOME/local/bin" ]; then 
+	mkdir -p "$HOME/local/bin"
+fi
+
+for i in ./.*
+do
+	ln -si "$PWD/${i##*/}" "$HOME/local/bin/${i##*/}"
+done
